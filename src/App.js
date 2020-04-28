1184b4df9259de30065ef82cc08cb41e5eaf89bd
Julian De Angelis <julian.deangelis@gmail.com>
2020-04-27 23:05:34 -0400

    Add test code for canvasjs library

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 import Chart from 'chart.js'
 import { Component } from 'react';
-// var CanvasJSReact = require('./canvasjs.react');
-// var CanvasJS = CanvasJSReact.CanvasJS;
-// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import CanvasJSReact from './canvasjs.react';
+//var CanvasJSReact = require('./canvasjs.react');
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class App extends Component {
 
@@ -74,7 +75,7 @@ class App extends Component {
                         display: true,
                         fontStyle: "bold",
                         fontColor: "black",
-                        labelString: "# of Active Cases"
+                        labelString: "Day"
                     },
                     type: "time",
                     time:{
@@ -82,11 +83,20 @@ class App extends Component {
                     }
                 }],
                 yAxes:[{
-                    type: "logarithmic",
+                    type: "linear",
+                    gridLines:{
+                        // display: false
+                    },
+                    scaleLabel: {
+                        display: true,
+                        fontStyle: "bold",
+                        fontColor: "black",
+                        labelString: "Total number of cases"
+                    },
                     ticks: {
-                        min: 0,
-                        max: 100000,
-                        // maxTicksLimit: 4,
+                        steps: 20,
+                        stepValue: 750,
+                        max: 15000,
                         callback: function(value, index, values) {//needed to change the scientific notation results from using logarithmic scale
                             return Number(value.toString());//pass tick values as a string into Number function
                         }
@@ -159,6 +169,43 @@ class App extends Component {
 
     render() {
 
+        const options = {
+			animationEnabled: true,
+			theme: "light2",
+			title: {
+				text: "Growth of Photovoltaics"
+			},
+			axisY: {
+				title: "Capacity (in MWp)",
+				logarithmic: true,
+				includeZero: false
+			},
+			data: [{
+				type: "spline",
+				showInLegend: true,
+				legendText: "MWp = one megawatt peak",
+				dataPoints: [
+				  { x: new Date(2001, 0), y: 1615},
+				  { x: new Date(2002, 0), y: 2069},
+				  { x: new Date(2003, 0), y: 2635},
+				  { x: new Date(2004, 0), y: 3723},
+				  { x: new Date(2005, 0), y: 5112},
+				  { x: new Date(2006, 0), y: 6660},
+				  { x: new Date(2007, 0), y: 9183},
+				  { x: new Date(2008, 0), y: 15844},
+				  { x: new Date(2009, 0), y: 23185},
+				  { x: new Date(2010, 0), y: 40336},
+				  { x: new Date(2011, 0), y: 70469},
+				  { x: new Date(2012, 0), y: 100504},
+				  { x: new Date(2013, 0), y: 138856},
+				  { x: new Date(2014, 0), y: 178391},
+				  { x: new Date(2015, 0), y: 229300},
+				  { x: new Date(2016, 0), y: 302300},
+				  { x: new Date(2017, 0), y: 405000}   
+				]
+			}]
+		}
+
         return (
             <div>
                 <canvas
@@ -166,6 +213,11 @@ class App extends Component {
                     ref={this.chartRef}
                     // style={{width: 800, height: 300}}
                 />
+
+                
+
+                {/* <CanvasJSChart options={options}/> */}
+                
             </div>
             
         )
@@ -173,26 +225,5 @@ class App extends Component {
     }
 
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
