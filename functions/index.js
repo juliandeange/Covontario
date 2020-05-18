a7e8df2367145248e4beafa210d2455f46d9792a
Julian De Angelis <julian.deangelis@gmail.com>
2020-05-18 12:45:49 -0400

    Change name of function

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,8 @@ const fetch = require("node-fetch");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.UpdateCaseData = functions.https.onRequest((request, response) => {
 
-        var json;
         fetch('https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&limit=100000')
             .then(response => response.json())
             .then(data => {
