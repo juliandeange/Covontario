0736c31923ddd20f6e500f3e87cbdb93f0304b83
Julian De Angelis <julian.deangelis@gmail.com>
2021-05-18 18:56:54 -0400

    Make script fully async
    Add column header and append rows for reference

--- src/query.js
+++ src/query.js
@@ -11,8 +11,11 @@ const httpsOptions = {
     })
   };
 
-// fetch('https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=1000000')
-fetch('https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=10', httpsOptions)
+
+  (async () => {
+
+// await fetch('https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=1000000', httpsOptions)
+    await fetch('https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=10', httpsOptions)
     .then(response => response.json())
     .then(data => {
 
@@ -29,10 +32,14 @@ fetch('https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455f
         console.log("Fatal Cases: " + fatal.value.length.toString())
         console.log("Active Cases: " + (data.result.total - resolved.value.length - fatal.value.length).toString())
 
+        AccessSpreadsheet()
+
     })
     .catch(err => {
         console.log(err)
     })
+
+})()
 
 async function AccessSpreadsheet() {
 
@@ -46,9 +53,30 @@ async function AccessSpreadsheet() {
 
     await doc.loadInfo()
 
-    console.log(doc.title)
+    const rows = await doc.sheetsByIndex[0].getRows()
+
+    // Date
+    // Total Cases
+    // New Cases
+    // 7 Day Average
+    // Resolved Cases
+    // New Recoveries
+    // Deceased Cases
+    // New Deaths
+    // Active Cases
+    // Active Case Difference
+    // New Tests
+    // % Positive
+    // Total Tests
+
+    // doc.sheetsByIndex[0].addRow({
+    //     Date: "1",
+    //     "Total Cases": "2"
+    // })
+
+    // console.log(rows.length)
 
 }
 
-AccessSpreadsheet()
+
     
