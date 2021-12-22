95e974d8dc657025836feff2ac7b2d65ec481fd5
Julian De Angelis <julian.deangelis@gmail.com>
2021-12-22 17:29:09 -0500

    Create requestor project
    Will be used to collect hospitalization data

---
+++ src/RequestorDB/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RequestorDB {
+    class Program {
+        static void Main(string[] args) {
+            
+            string url = "https://data.ontario.ca/api/3/action/datastore_search?resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11&limit=1000000"
+
+        }
+    }
+}
