e3988d7d5a33069f267cbe5dcf2136c43103e9ec
Julian De Angelis <julian.deangelis@gmail.com>
2022-04-05 18:53:14 -0400

    Refactor GraphHospitalizations.js
    - Convert class to functional style
    - Use hooks

--- src/GraphHospitalizations.js
+++ src/GraphHospitalizations.js
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Chart from 'chart.js'
-import { Component } from 'react'
-import { isBrowser } from "react-device-detect"
+import { isBrowser } from 'react-device-detect'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { LocalHospital } from '@mui/icons-material'
 import Snackbar from '@mui/material/Snackbar'
@@ -10,74 +9,21 @@ import Snackbar from '@mui/material/Snackbar'
 // eslint-disable-next-line
 import Zoom from 'chartjs-plugin-zoom'
 
-class GraphHospitalizations extends Component { 
+function GraphHospitalizations(data) {
 
-    constructor(props) {
-        super(props)
-        this.state = {
+    const chartRef = React.createRef();
+    
+    const [snackOpen, setSnackOpen] = useState(false)    
 
-            snackOpen: false
-
-        }
-
-    }
-
-    chartRef = React.createRef();
-
-    filterUndefined(i) {
+    const filterUndefined = (i) => {
 
         return i !== undefined
 
     }
-    
-    setAxis(chart) {
 
-        // Get date range shown
-        var startEpoch = chart.chart.scales["X"]._ticks[0].value
-        var endEpoch = chart.chart.scales["X"]._ticks[chart.chart.scales["X"]._ticks.length - 1].value
+    useEffect(() => { 
 
-        var startDate = new Date(startEpoch)
-        var endDate = new Date(endEpoch)
-
-        var startDateString = startDate.toLocaleString('default', { month: 'long' }) + ' ' + (startDate.getDate() < 10 ? '0' + startDate.getDate() : startDate.getDate()) + ' ' + startDate.getFullYear()
-        var endDateString = endDate.toLocaleString('default', { month: 'long' }) + ' ' + (endDate.getDate() < 10 ? '0' + endDate.getDate() : endDate.getDate()) + ' ' + endDate.getFullYear()
-
-        var startIndex = this.props.data.findIndex(i => i.Date === startDateString)
-        var endIndex = this.props.data.findIndex(i => i.Date === endDateString)
-
-        // Get range of dates shown
-        var range = this.props.data.slice(startIndex === -1 ? 0 : startIndex, endIndex === -1 ? this.props.data.length : endIndex + 1)
-
-        // Get max values for each axis
-        var maxHos = Math.max(...range.map(i => i["Hospitalizations"]).filter(this.filterUndefined))
-        var maxICU = Math.max(...range.map(i => i["ICU"]).filter(this.filterUndefined))
-        var maxICUVent = Math.max(...range.map(i => i["ICU_Ventilated"]).filter(this.filterUndefined))
-
-        var maxAxis = Math.max(maxHos, maxICU, maxICUVent)
-
-        var limit = maxAxis
-        
-        // Round the axis values
-        if (limit < 100)
-            limit = Math.ceil(maxAxis / 5) * 5
-        else if (limit < 1000)
-            limit = Math.ceil(maxAxis / 50) * 50
-        else if (limit < 10000)
-            limit = Math.ceil(maxAxis / 500) * 500
-        else if (limit < 100000)
-            limit = Math.ceil(maxAxis / 5000) * 5000
-        else if (limit < 1000000)
-            limit = Math.ceil(maxAxis / 50000) * 50000
-
-        // Set the new axis
-        chart.chart.options.scales.yAxes[0].ticks.max = limit
-        chart.chart.update()
-
-    }
-
-    async componentDidMount() {
-
-        const myChartRef = this.chartRef.current.getContext("2d");
+        const myChartRef = chartRef.current.getContext("2d");
 
         var currentDate = new Date();
         var daysBack = new Date(currentDate);
@@ -149,27 +95,27 @@ class GraphHospitalizations extends Component {
             pan: {
                 enabled: true,
                 mode: 'x',
-                onPanComplete: this.setAxis.bind(this)
+                onPanComplete: setAxis
             },
             zoom: {
                 enabled: true,                      
                 mode: 'x',
                 speed: 0.04, // 4%
-                onZoomComplete: this.setAxis.bind(this)
+                onZoomComplete: setAxis
             },
         }
 
         var chart = new Chart(myChartRef, {
             type: "line",
                 data: {
-                    labels: this.props.data.map((key, index) => { return this.props.data[index]["Date"]}),
+                    labels: data.data.map((key, index) => { return data.data[index]["Date"]}),
                     datasets: [
                         {
                             borderColor: "#ffff99",
                             fill: false,
                             label: "Hospitalizations",
                             yAxisId: "Hospitalizations",
-                            data: this.props.data.map((key, index) => { return this.props.data[index]["Hospitalizations"]}),
+                            data: data.data.map((key, index) => { return data.data[index]["Hospitalizations"]}),
                             pointRadius: 5
                         },
                         {
@@ -177,7 +123,7 @@ class GraphHospitalizations extends Component {
                             fill: false,
                             label: "ICU",
                             yAxisID: "Hospitalizations",
-                            data: this.props.data.map((key, index) => { return this.props.data[index]["ICU"]}),
+                            data: data.data.map((key, index) => { return data.data[index]["ICU"]}),
                             pointRadius: 5
                         },
                         {
@@ -185,7 +131,7 @@ class GraphHospitalizations extends Component {
                             fill: false,
                             label: "ICU + Ventilated",
                             yAxisID: "Hospitalizations",
-                            data: this.props.data.map((key, index) => {return this.props.data[index]["ICU_Ventilated"]}),
+                            data: data.data.map((key, index) => {return data.data[index]["ICU_Ventilated"]}),
                             pointRadius: 5
                         }
 
@@ -195,42 +141,84 @@ class GraphHospitalizations extends Component {
         
         });
 
-        this.setAxis(chart)
-        this.setState({ snackOpen: true })
+        setAxis(chart)
+        setSnackOpen(true)
+
+    // eslint-disable-next-line
+    }, [])
+    
+    const setAxis = (chart) => {
+
+        // Get date range shown
+        var startEpoch = chart.chart.scales["X"]._ticks[0].value
+        var endEpoch = chart.chart.scales["X"]._ticks[chart.chart.scales["X"]._ticks.length - 1].value
+
+        var startDate = new Date(startEpoch)
+        var endDate = new Date(endEpoch)
+
+        var startDateString = startDate.toLocaleString('default', { month: 'long' }) + ' ' + (startDate.getDate() < 10 ? '0' + startDate.getDate() : startDate.getDate()) + ' ' + startDate.getFullYear()
+        var endDateString = endDate.toLocaleString('default', { month: 'long' }) + ' ' + (endDate.getDate() < 10 ? '0' + endDate.getDate() : endDate.getDate()) + ' ' + endDate.getFullYear()
+
+        var startIndex = data.data.findIndex(i => i.Date === startDateString)
+        var endIndex = data.data.findIndex(i => i.Date === endDateString)
+
+        // Get range of dates shown
+        var range = data.data.slice(startIndex === -1 ? 0 : startIndex, endIndex === -1 ? data.data.length : endIndex + 1)
+
+        // Get max values for each axis
+        var maxHos = Math.max(...range.map(i => i["Hospitalizations"]).filter(filterUndefined))
+        var maxICU = Math.max(...range.map(i => i["ICU"]).filter(filterUndefined))
+        var maxICUVent = Math.max(...range.map(i => i["ICU_Ventilated"]).filter(filterUndefined))
+
+        var maxAxis = Math.max(maxHos, maxICU, maxICUVent)
+
+        var limit = maxAxis
+        
+        // Round the axis values
+        if (limit < 100)
+            limit = Math.ceil(maxAxis / 5) * 5
+        else if (limit < 1000)
+            limit = Math.ceil(maxAxis / 50) * 50
+        else if (limit < 10000)
+            limit = Math.ceil(maxAxis / 500) * 500
+        else if (limit < 100000)
+            limit = Math.ceil(maxAxis / 5000) * 5000
+        else if (limit < 1000000)
+            limit = Math.ceil(maxAxis / 50000) * 50000
+
+        // Set the new axis
+        chart.chart.options.scales.yAxes[0].ticks.max = limit
+        chart.chart.update()
 
     }
 
-    handleSnackClose() {
+    const handleSnackClose = () => {
 
-        this.setState({ snackOpen: false })
+        setSnackOpen(false)
 
     }
-
-    render() {
-
-        return(
-            <div>
-                {this.props.data.length <= 0 ? 
-                    <div style={{position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
-                        <CircularProgress color="secondary"/> 
-                    </div> : 
-                    null
-                }
-                <div style={{height: !isBrowser ? "calc(90vh - 10vh)" : "90vh"}}>
-                    <canvas
-                        id="myChart"
-                        ref={this.chartRef}
-                        style={{height: !isBrowser ? "95%" : "100%", width: "100%"}}/>
-                </div>  
-                <Snackbar
-                    open={this.state.snackOpen}
-                    autoHideDuration={3000}
-                    onClose={this.handleSnackClose.bind(this)}
-                    message={<span style={{fontWeight: 'bolder'}}><LocalHospital style={{marginBottom: -6, marginRight: 10}} />Hospitalizations</span>}
-                />         
-            </div>
-        )
-    }
+    return(
+        <div>
+            {data.data.length <= 0 ? 
+                <div style={{position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
+                    <CircularProgress color="secondary"/> 
+                </div> : 
+                null
+            }
+            <div style={{height: !isBrowser ? "calc(90vh - 10vh)" : "90vh"}}>
+                <canvas
+                    id="myChart"
+                    ref={chartRef}
+                    style={{height: !isBrowser ? "95%" : "100%", width: "100%"}}/>
+            </div>  
+            <Snackbar
+                open={snackOpen}
+                autoHideDuration={3000}
+                onClose={handleSnackClose}
+                message={<span style={{fontWeight: 'bolder'}}><LocalHospital style={{marginBottom: -6, marginRight: 10}} />Hospitalizations</span>}
+            />         
+        </div>
+    )
 }
 
 export default GraphHospitalizations
