cea8af8ed416ca139d49aea2df39df382f59c816
Julian De Angelis <julian.deangelis@gmail.com>
2023-03-20 17:45:00 -0400

    Fetch data from Ontario API
    - Parse JSON into variable

--- script/ruby_query/query.rb
+++ script/ruby_query/query.rb
@@ -1,11 +1,17 @@
 #!/usr/bin/env ruby
 
 require 'net/http'
+# require 'rest_client'
+require 'json'
+require 'uri'
 
-# name = ARGV[0]
-# puts "Hello #{name}!"
+url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=100'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+json = JSON.parse(response)
 
-uri = URI('https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=100')
-data = Net::HTTP.get(uri) # => String
+data = json['result']['records']
+
+puts data
 
 
