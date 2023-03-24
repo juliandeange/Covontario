a18d84fda0e5082eccb876528907ae775b78f575
Julian De Angelis <julian.deangelis@gmail.com>
2023-03-24 08:57:23 -0400

    Create main and spreadsheet functions

--- script/ruby_query/query.rb
+++ script/ruby_query/query.rb
@@ -5,30 +5,45 @@ require 'net/http'
 require 'json'
 require 'uri'
 
-url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=10000000'
-uri = URI(url)
-response = Net::HTTP.get(uri)
-# puts response;
+def GetData
+    url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=100'
+    # url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=10000000'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    # puts response;
 
-json = JSON.parse(response)
-records = json['result']['records']
-total = json['result']['total']
+    json = JSON.parse(response)
+    records = json['result']['records']
+    total = json['result']['total']
 
-resolvedCases = 0
-fatalCases = 0
+    resolvedCases = 0
+    fatalCases = 0
 
-records.each do |child|
-    if child['Outcome1'] == 'Resolved'
-        resolvedCases = resolvedCases + 1
-    elsif child['Outcome1'] == 'Fatal'
-        fatalCases = fatalCases + 1
+    records.each do |child|
+        if child['Outcome1'] == 'Resolved'
+            resolvedCases = resolvedCases + 1
+        elsif child['Outcome1'] == 'Fatal'
+            fatalCases = fatalCases + 1
+        end
     end
+
+    puts "Total Cases: #{total}"
+    puts "Resolved Cases: #{resolvedCases}"
+    puts "Fatal Cases: #{fatalCases}"
+    puts "Active Cases: #{total - resolvedCases - fatalCases}"
+
+    AccessSpreadsheet(total, resolvedCases, fatalCases)
+
+    puts 'done'
 end
 
-puts "Total Cases: #{total}"
-puts "Resolved Cases: #{resolvedCases}"
-puts "Fatal Cases: #{fatalCases}"
-puts "Active Cases: #{total - resolvedCases - fatalCases}"
 
-puts 'done'
+def AccessSpreadsheet(total, resolved, fatal)
+   
+    puts Time.new
 
+end
+
+
+
+GetData()
