986013685a3aaabb187be30da56e8bf90ae09222
Julian De Angelis <julian.deangelis@gmail.com>
2023-04-01 16:40:03 -0400

    Begin refactor with new Ontario Data API
    - Create WriteObjectToSpreadsheet function

--- script/ruby_query/query.rb
+++ script/ruby_query/query.rb
@@ -10,7 +10,8 @@ Bundler.require
 
 def GetData
     # url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=100'
-    url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=10000000'
+    # url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=10000000'
+    url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11&limit=20&sort=Reported%20Date%20desc'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     # puts response;
@@ -19,30 +20,40 @@ def GetData
     records = json['result']['records']
     total = json['result']['total']
 
-    resolvedCases = 0
-    fatalCases = 0
+    # records.reverse!
 
-    records.each do |child|
-        if child['Outcome1'] == 'Resolved'
-            resolvedCases = resolvedCases + 1
-        elsif child['Outcome1'] == 'Fatal'
-            fatalCases = fatalCases + 1
-        end
-    end
+    # resolvedCases = 0
+    # fatalCases = 0
 
-    puts "Total Cases: #{total}"
-    puts "Resolved Cases: #{resolvedCases}"
-    puts "Fatal Cases: #{fatalCases}"
-    puts "Active Cases: #{total - resolvedCases - fatalCases}"
-    puts "----------"
+    # records.each do |data|
+    #     time = Time.new(data['Reported Date'])
+    #     date = time.strftime('%B %d %Y')
+        
+    #     # if (date)
+    #     # if child['Outcome1'] == 'Resolved'
+    #     #     resolvedCases = resolvedCases + 1
+    #     # elsif child['Outcome1'] == 'Fatal'
+    #     #     fatalCases = fatalCases + 1
+    #     # end
+    # end
 
-    # AccessSpreadsheet(total, resolvedCases, fatalCases)
+
+
+    # puts "Total Cases: #{total}"
+    # puts "Resolved Cases: #{resolvedCases}"
+    # puts "Fatal Cases: #{fatalCases}"
+    # puts "Active Cases: #{total - resolvedCases - fatalCases}"
+    # puts "----------"
+
+    AccessSpreadsheet(records)
 
     puts 'done'
 end
 
 
-def AccessSpreadsheet(total, resolved, fatal)
+def AccessSpreadsheet(data)
+
+    # puts data
 
     # Create google sheet context
     session = GoogleDrive::Session.from_service_account_key("../client_secret.json")
@@ -60,27 +71,58 @@ def AccessSpreadsheet(total, resolved, fatal)
         count = count + 1
     end
 
+    puts headerRow
 
-    # puts rows[totalRows - 1][headerRow["Total Cases"]]
-    today = Time.new().strftime('%B %d %Y')
-    yesterday = rows[totalRows - 1][headerRow["Date"]]
+    data.reverse!
+    beginInsert = false
 
-    yesterdayTotalCases = rows[totalRows - 1][headerRow["Total Cases"]].to_i
-    yesterdayRecoveries = rows[totalRows - 1][headerRow["Resolved Cases"]].to_i
-    yesterdayFatal= rows[totalRows - 1][headerRow["Deceased Cases"]].to_i
-
-    if yesterday != today && yesterdayTotalCases != total && total >= yesterdayTotalCases
-        
-        activeCases = total - resolved - fatal
-        newCases = total - yesterdayTotalCases
-        newRecoveries = resolved - rows[totalRows - 1][headerRow["Resolved Cases"]].to_i
-        newFatal = fatal - rows[totalRows - 1][headerRow["Deceased Cases"]].to_i
-        activeCaseDifference = activeCases - rows[totalRows - 1][headerRow["Active Cases"]].to_i
-
-        worksheet.insert_rows(totalRows + 1, [[today, total, newCases, "", resolved, newRecoveries, fatal, newFatal, activeCases, activeCaseDifference]])
-        worksheet.save
+    data.each do |d|
+        time = Time.new(d['Reported Date'])
+        date = time.strftime('%B %d %Y')
+        if rows[totalRows - 1][headerRow['Date']] == date
+            beginInsert = true
+            next
+        elsif beginInsert == true
+            obj = CreateCaseDataObject(d)
+            WriteObjectToSpreadsheet(obj, worksheet)
+        end
 
     end
+    
+
+    # # puts rows[totalRows - 1][headerRow["Total Cases"]]
+    # today = Time.new().strftime('%B %d %Y')
+    # yesterday = rows[totalRows - 1][headerRow["Date"]]
+
+    # yesterdayTotalCases = rows[totalRows - 1][headerRow["Total Cases"]].to_i
+    # yesterdayRecoveries = rows[totalRows - 1][headerRow["Resolved Cases"]].to_i
+    # yesterdayFatal= rows[totalRows - 1][headerRow["Deceased Cases"]].to_i
+
+    # if yesterday != today && yesterdayTotalCases != total && total >= yesterdayTotalCases
+        
+    #     activeCases = total - resolved - fatal
+    #     newCases = total - yesterdayTotalCases
+    #     newRecoveries = resolved - rows[totalRows - 1][headerRow["Resolved Cases"]].to_i
+    #     newFatal = fatal - rows[totalRows - 1][headerRow["Deceased Cases"]].to_i
+    #     activeCaseDifference = activeCases - rows[totalRows - 1][headerRow["Active Cases"]].to_i
+
+    #     worksheet.insert_rows(totalRows + 1, [[today, total, newCases, "", resolved, newRecoveries, fatal, newFatal, activeCases, activeCaseDifference]])
+    #     worksheet.save
+
+    # end
+
+end
+
+def WriteObjectToSpreadsheet(data, sheet)
+
+    rows = sheet.rows
+    totalRows = sheet.rows.length
+
+    # sheet.insert_rows(totalRows + 1, [[data.date, data.total, data.]])
+    # sheet.save
+
+end
+
 def CreateCaseDataObject(data)
 
     date = Time.new(data['Reported Date']).strftime('%B %d %Y')
