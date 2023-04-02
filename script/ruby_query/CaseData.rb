bad2955dc2cce4c2366883179e5e5ec79f79c7c5
Julian De Angelis <julian.deangelis@gmail.com>
2023-04-02 13:27:19 -0400

    Add hospitalization data to object

--- script/ruby_query/CaseData.rb
+++ script/ruby_query/CaseData.rb
@@ -4,9 +4,14 @@
 #   - total resolved cases
 #   - total fatalities
 #   - current active case count
+#   - new tests completed
+#   - total tests completed
+#   - current hospitalizations
+#   - current icu cases
+#   - current icu + ventilated cases
 class CaseData
     attr_reader :date, :total, :resolved, :fatal, :active
-    def initialize(date, total, res, fatal, active, newTests, totalTests)
+    def initialize(date, total, res, fatal, active, newTests, totalTests, hospital, icu, icuVented)
         @date = date
         @total = total
         @resolved = res
@@ -14,5 +19,8 @@ class CaseData
         @active = active
         @newTests = newTests
         @totalTests = totalTests
+        @hospital = hospital
+        @icu = icu
+        @icuVented = icuVented
     end
 end
