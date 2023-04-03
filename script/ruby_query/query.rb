807f571ef0c5234955153d0d7a848b098cdb8a8b
Julian De Angelis <julian.deangelis@gmail.com>
2023-04-02 23:12:32 -0400

    Backfill all data from last absent date
    - Iterate over API data and bulk insert all rows into the spreadsheet
    - Query last 14 days

--- script/ruby_query/query.rb
+++ script/ruby_query/query.rb
@@ -11,7 +11,7 @@ Bundler.require
 def GetData
     # url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=100'
     # url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&fields=Outcome1&limit=10000000'
-    url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11&limit=20&sort=Reported%20Date%20desc'
+    url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11&limit=14&sort=Reported%20Date%20desc'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     # puts response;
@@ -84,10 +84,13 @@ def AccessSpreadsheet(data)
             next
         elsif beginInsert == true
             obj = CreateCaseDataObject(d)
-            WriteObjectToSpreadsheet(obj, worksheet)
+            WriteObjectToSpreadsheet(obj, worksheet, headerRow)
+            # PrintCaseDataObject(obj)
         end
 
     end
+
+    worksheet.save
     
 
     # # puts rows[totalRows - 1][headerRow["Total Cases"]]
@@ -113,12 +116,62 @@ def AccessSpreadsheet(data)
 
 end
 
-def WriteObjectToSpreadsheet(data, sheet)
+def WriteObjectToSpreadsheet(data, sheet, headerRow)
 
     rows = sheet.rows
     totalRows = sheet.rows.length
 
-    # sheet.insert_rows(totalRows + 1, [[data.date, data.total, data.]])
+    newCases = data.total - rows[totalRows - 1][headerRow['Total Cases']].to_i
+    newRecoveries = data.resolved - rows[totalRows - 1][headerRow['Resolved Cases']].to_i
+    newFatalities = data.fatal - rows[totalRows - 1][headerRow['Deceased Cases']].to_i
+    activeCaseDifference = data.active - rows[totalRows - 1][headerRow["Active Cases"]].to_i
+
+    # Date
+    # Total Cases  
+    # New Cases
+    # 7 Day Average	
+    # Resolved Cases	
+    # New Recoveries	
+    # Deceased Cases	
+    # New Deaths	
+    # Active Cases	
+    # Active Case Difference	
+    # New Tests
+    # % Positive	
+    # Total Tests		
+    # *** SPARE ***
+    # Cases_NotFullyVaccinated	
+    # Cases_Vax	
+    # Cases_Boosted	
+    # Cases_Unknown	
+    # Hospitalizations	
+    # ICU	ICU_Ventilated	
+    # Cases_Unvax
+    # Cases_Partial										
+
+    sheet.insert_rows(totalRows + 1, [[
+        data.date,
+        data.total,
+        newCases,
+        '',
+        data.resolved,
+        newRecoveries,
+        data.fatal,
+        newFatalities,
+        data.active,
+        activeCaseDifference,
+        data.newTests,
+        '',
+        data.totalTests,
+        '',
+        '',
+        '',
+        '',
+        '',
+        data.hospital,
+        data.icu,
+        data.icuVented
+    ]])
     # sheet.save
 
 end
@@ -132,8 +185,11 @@ def CreateCaseDataObject(data)
     active = data['Confirmed Positive']
     newTests = data['Total tests completed in the last day']
     totalTests = data['Total patients approved for testing as of Reporting Date']
+    hospitalizations = data['Number of patients hospitalized with COVID-19']
+    icu = data['Number of patients in ICU due to COVID-19']
+    icuVented = data['Number of patients in ICU on a ventilator due to COVID-19']
 
-    return CaseData.new(date, total, resolved, fatal, active, newTests, totalTests)
+    return CaseData.new(date, total, resolved, fatal, active, newTests, totalTests, hospitalizations, icu, icuVented)
 
 end
 
