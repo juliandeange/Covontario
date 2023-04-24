64743874a25ac63d4541658f4c3aca7ff5ef341f
Julian De Angelis <julian.deangelis@gmail.com>
2023-04-23 23:34:08 -0400

    Create CaseData.js

---
+++ script/node_query/CaseData.js
@@ -0,0 +1,19 @@
+class CaseData {
+
+    constructor(date, total, res, fatal, active, newTests, totalTests, hospital, icu, icuVented) {
+
+        this.date = date
+        this.total = total
+        this.resolved = res
+        this.fatal = fatal
+        this.active = active
+        this.newTests = newTests
+        this.totalTests = totalTests
+        this.hospital = hospital
+        this.icu = icu
+        this.icuVented = icuVented
+
+    }
+}
+
+// export default CaseData
